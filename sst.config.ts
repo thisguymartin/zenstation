6b9bc131e0f65c908aa037351314facc70b51873
Martin Patino <mpatino117@gmail.com>
2024-09-18 08:03:22 -0700

    chore: code

--- sst.config.ts
+++ sst.config.ts
@@ -15,21 +15,11 @@ export default $config({
     };
   },
   async run() {
-    // $transform(cloudflare.WorkerScript, (script) => {
-    //   script.logpush = true;
-    // });
-    // sst.Linkable.wrap(cloudflare.Record, function (record) {
-    //   return {
-    //     properties: {
-    //       url: $interpolate`https://${record.name}`,
-    //     },
-    //   };
-    // });
-    // const outputs = {};
-    // for (const value of readdirSync("./infra/")) {
-    //   const result = await import("./infra/" + value);
-    //   if (result.outputs) Object.assign(outputs, result.outputs);
-    // }
-    // return outputs;
+    const outputs = {};
+    for (const value of readdirSync("./infra/")) {
+      const result = await import("./infra/" + value);
+      if (result.outputs) Object.assign(outputs, result.outputs);
+    }
+    return outputs;
   },
 });
